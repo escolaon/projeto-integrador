73b46cd0efd513626bd29eecd17b3e540b29efa7
Mauricio Bellon <mauriciobellon@gmail.com>
2024-04-27 19:30:17 -0300

    from start

--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the home page</p>
-        <NuxtLink to="/login">Entrar</NuxtLink>
-    </div>
-</template>
--- pages/login.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>Login Page</h1>
-        <p></p>
-        <NuxtLink to="/">Inicio</NuxtLink>
-    </div>
-</template>
