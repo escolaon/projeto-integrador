f8f84abc0e95155a40bba6cd3deb8ef6f3dbc84b
Mauricio Bellon <mauriciobellon@gmail.com>
2024-04-29 21:20:16 -0300

    Update Nuxt configuration, fix navbar styling, and add new pages and components

--- pages/cadastrar.vue
+++ pages/cadastrar.vue
@@ -59,70 +59,51 @@ function onSubmit(event: Event) {
 
 <template>
   <div>
-    <div class="flex h-dvh items-center justify-center">
-      <UiCard
-        class="relative m-4 grid h-[550px] w-full grid-cols-1 place-content-center rounded-lg border p-4 shadow-2xl sm:w-[508px]">
-        <div class="absolute inset-x-0 top-0 flex items-center justify-center rounded-t-lg bg-black p-1 shadow-lg">
-        </div>
-
-        <UiButton variant="ghost" class="absolute right-8 top-8 hover:bg-accent hover:text-accent-foreground">
-          <NuxtLink to="/entrar">Entrar</NuxtLink>
-        </UiButton>
-
-        <div class="flex items-center justify-center">
-          <Icon name="heroicons-solid:lock-closed" class="h-16 w-16 text-accent" />
-        </div>
-
-        <UiCardHeader class="flex flex-col space-y-4 text-center">
-          <UiCardTitle class="text-2xl">Faça seu Cadastro</UiCardTitle>
-          <UiCardDescription>
-            <p v-if='verNome'>Digite seu nome para começarmos.</p>
-            <p v-if="verEmail">Digite seu email para continuar.</p>
-            <p v-if="verSenha">Digite sua senha para Cadastrar.</p>
-          </UiCardDescription>
-        </UiCardHeader>
-        <UiCardContent>
-          <form @submit="onSubmit">
-            <div class="grid w-full items-center gap-4">
-              <div v-if="verNome" class="flex flex-col space-y-1.5">
-                <UiLabel class="sr-only" for="nome"> Nome </UiLabel>
-                <UiInput v-model="nome" id="nome" placeholder="Fulando de Tal" type="text" auto-capitalize="none"
-                  auto-complete="name" auto-correct="off" class="h-12" :disabled="carregando" />
-              </div>
-              <div v-if="verEmail" class="flex flex-col space-y-1.5">
-                <UiLabel class="sr-only" for="email"> Email </UiLabel>
-                <UiInput v-model="email" id="email" placeholder="name@example.com" type="email" auto-capitalize="none"
-                  auto-complete="email" auto-correct="off" class="h-12" :disabled="carregando" />
-              </div>
-              <div v-if="verSenha" class="flex flex-col space-y-1.5">
-                <UiLabel class="sr-only" for="email"> Senha </UiLabel>
-                <UiInput v-model="senha" id="senha" placeholder="********" type="password" auto-capitalize="none"
-                  auto-complete="password" auto-correct="off" class="h-12" :disabled="carregando" />
-              </div>
-              <UiButton type="submit" :disabled="carregando" class="h-12">
-                <Icon name="lucide:loader-2" color="white" v-if="carregando" class="mr-2 h-4 w-4 animate-spin" />
-                <p v-if="verNome">Continuar</p>
-                <p v-if="verEmail">Continuar</p>
-                <p v-if="verSenha">Cadastrar</p>
-              </UiButton>
-            </div>
-          </form>
-        </UiCardContent>
-        <UiCardFooter class="text-center text-sm text-muted-foreground flex-none grid">
-          <p class="w-full">
-            Clicando em continuar, você concorda com nossos
-          </p>
-          <p>
-            <NuxtLink to="/termos" class="underline underline-offset-4 hover:text-primary">
-              Termos de Serviço
-            </NuxtLink>
-            e
-            <NuxtLink to="/politicas" class="underline underline-offset-4 hover:text-primary">
-              Política de Privacidade
-            </NuxtLink>
-          </p>
-        </UiCardFooter>
-      </UiCard>
+    <div class="flex items-center justify-center">
+      <Icon name="heroicons-solid:lock-closed" class="h-16 w-16 text-accent" />
     </div>
+    <UiCardHeader class="flex flex-col space-y-4 text-center">
+      <UiCardTitle class="text-2xl">Faça seu Cadastro</UiCardTitle>
+      <UiCardDescription>
+        <p v-if='verNome'>Digite seu nome para começarmos.</p>
+        <p v-if="verEmail">Digite seu email para continuar.</p>
+        <p v-if="verSenha">Digite sua senha para Cadastrar.</p>
+      </UiCardDescription>
+    </UiCardHeader>
+    <UiCardContent>
+      <form @submit="onSubmit">
+        <div class="grid w-full items-center gap-4">
+          <div v-if="verNome" class="flex flex-col space-y-1.5">
+            <UiLabel class="sr-only" for="nome"> Nome </UiLabel>
+            <UiInput v-model="nome" id="nome" placeholder="Fulando de Tal" type="text" auto-capitalize="none"
+              auto-complete="name" auto-correct="off" class="h-12" :disabled="carregando" />
+          </div>
+          <div v-if="verEmail" class="flex flex-col space-y-1.5">
+            <UiLabel class="sr-only" for="email"> Email </UiLabel>
+            <UiInput v-model="email" id="email" placeholder="name@example.com" type="email" auto-capitalize="none"
+              auto-complete="email" auto-correct="off" class="h-12" :disabled="carregando" />
+          </div>
+          <div v-if="verSenha" class="flex flex-col space-y-1.5">
+            <UiLabel class="sr-only" for="email"> Senha </UiLabel>
+            <UiInput v-model="senha" id="senha" placeholder="********" type="password" auto-capitalize="none"
+              auto-complete="password" auto-correct="off" class="h-12" :disabled="carregando" />
+          </div>
+          <UiButton type="submit" :disabled="carregando" class="h-12">
+            <Icon name="lucide:loader-2" color="white" v-if="carregando" class="mr-2 h-4 w-4 animate-spin" />
+            <p v-if="verNome">Continuar</p>
+            <p v-if="verEmail">Continuar</p>
+            <p v-if="verSenha">Cadastrar</p>
+          </UiButton>
+        </div>
+      </form>
+    </UiCardContent>
+    <UiCardFooter class="text-center text-sm text-muted-foreground flex-none grid">
+      <p class="w-full">
+        <NuxtLink to="/entrar" class="underline underline-offset-4 hover:text-primary">
+          Já tenho conta!
+        </NuxtLink>
+      </p>
+    </UiCardFooter>
+
   </div>
 </template>
--- pages/entrar.vue
+++ pages/entrar.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
+import { ref } from "vue";
 
 definePageMeta({
-    layout: 'nonav'
+    layout: "nonav",
 });
 
-
-import { ref } from "vue";
 const email = ref("");
 const senha = ref("");
 const verEmail = ref(true);
@@ -41,83 +40,53 @@ function onSubmit(event: Event) {
         }
     }
 }
-
 </script>
 
 <template>
     <div>
-        <div class="flex h-dvh items-center justify-center">
-            <UiCard
-                class="relative m-4 grid min-h-[550px] w-full grid-cols-1 place-content-center rounded-lg border p-4 shadow-xl sm:w-[508px]">
-
-
-                <div
-                    class="absolute inset-x-0 top-0 flex items-center justify-center rounded-t-lg bg-[#09090b] p-1 shadow-xl">
-                </div>
-
-                <UiButton variant="ghost" class="absolute right-8 top-8 hover:bg-accent hover:text-accent-foreground"
-                    @click="navigateTo('/cadastrar')">
-                    Cadastrar
-                </UiButton>
-
-                <UiButton @click="$colorMode.preference = $colorMode.value == 'dark' ? 'light' : 'dark'" variant="ghost"
-                    size="icon-sm" class="absolute left-8 top-8 hover:bg-accent hover:text-accent-foreground">
-                    <Icon name="lucide:sun-medium" class="h-4 w-4" />
-                </UiButton>
-
-                <div class="flex items-center justify-center">
-                    <Icon name="heroicons-solid:lock-closed" class="h-16 w-16 text-accent" />
-                </div>
-
-                <UiCardHeader class="flex flex-col space-y-4 text-center">
-                    <UiCardTitle class="text-2xl">Faça seu Login</UiCardTitle>
-                    <UiCardDescription>
-                        <p v-if="verEmail">Digite seu email para continuar.</p>
-                        <p v-if="verSenha">Digite sua senha para Entrar.</p>
-                    </UiCardDescription>
-                </UiCardHeader>
-                <UiCardContent>
-                    <form @submit="onSubmit">
-                        <div class="grid w-full items-center gap-4">
-                            <div v-if="verEmail" class="flex flex-col space-y-1.5">
-                                <UiLabel class="sr-only" for="email"> Email </UiLabel>
-                                <UiInput v-model="email" id="email" placeholder="name@example.com" type="email"
-                                    auto-capitalize="none" auto-complete="email" auto-correct="off" class="h-12"
-                                    :disabled="carregando" />
-                            </div>
-                            <div v-if="verSenha" class="flex flex-col space-y-1.5">
-                                <UiLabel class="sr-only" for="email"> Senha </UiLabel>
-                                <UiInput v-model="senha" id="senha" placeholder="********" type="password"
-                                    auto-capitalize="none" auto-complete="password" auto-correct="off" class="h-12"
-                                    :disabled="carregando" />
-                            </div>
-                            <UiButton type="submit" :disabled="carregando" class="h-12">
-                                <Icon name="lucide:loader-2" color="white" v-if="carregando"
-                                    class="mr-2 h-4 w-4 animate-spin" />
-                                <p v-if="verEmail">Continuar</p>
-                                <p v-if="verSenha">Entrar</p>
-                            </UiButton>
-                        </div>
-                    </form>
-                </UiCardContent>
-                <UiCardFooter class="text-center text-sm text-muted-foreground">
-                    <p class="w-full">
-                        Esqueceu sua senha?
-                        <NuxtLink to="/lembrar" class="underline underline-offset-4 hover:text-primary">
-                            Clique aqui
-                        </NuxtLink>
-                        .
-                    </p>
-                </UiCardFooter>
-
-                <!-- <div class="absolute
-                h-full
-                w-full
-                bg-[radial-gradient(theme(colors.border)_1px,transparent_2px)]
-                [background-size:20px_20px]
-                [mask-image:radial-gradient(ellipse_600px_600px_at_50%_50%,#000_50%,transparent_100%)]
-                md:[mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_50%,transparent_100%)]" /> -->
-            </UiCard>
+        <div class="flex items-center justify-center">
+            <Icon name="heroicons-solid:lock-closed" class="h-16 w-16 text-accent" />
         </div>
+        <UiCardHeader class="flex flex-col space-y-4 text-center">
+            <UiCardTitle class="text-2xl">Faça seu Login</UiCardTitle>
+            <UiCardDescription>
+                <p v-if="verEmail">Digite seu email para continuar.</p>
+                <p v-if="verSenha">Digite sua senha para Entrar.</p>
+            </UiCardDescription>
+        </UiCardHeader>
+        <UiCardContent>
+            <form @submit="onSubmit">
+                <div class="grid w-full items-center gap-4">
+                    <div v-if="verEmail" class="flex flex-col space-y-1.5">
+                        <UiLabel class="sr-only" for="email"> Email </UiLabel>
+                        <UiInput v-model="email" id="email" placeholder="name@example.com" type="email"
+                            auto-capitalize="none" auto-complete="email" auto-correct="off" class="h-12"
+                            :disabled="carregando" />
+                    </div>
+                    <div v-if="verSenha" class="flex flex-col space-y-1.5">
+                        <UiLabel class="sr-only" for="email"> Senha </UiLabel>
+                        <UiInput v-model="senha" id="senha" placeholder="********" type="password"
+                            auto-capitalize="none" auto-complete="password" auto-correct="off" class="h-12"
+                            :disabled="carregando" />
+                    </div>
+                    <UiButton type="submit" :disabled="carregando" class="h-12">
+                        <Icon name="lucide:loader-2" color="white" v-if="carregando"
+                            class="mr-2 h-4 w-4 animate-spin" />
+                        <p v-if="verEmail">Continuar</p>
+                        <p v-if="verSenha">Entrar</p>
+                    </UiButton>
+                </div>
+            </form>
+        </UiCardContent>
+        <UiCardFooter class="text-center text-sm text-muted-foreground">
+            <p class="flex justify-between w-full">
+                <NuxtLink to="/cadastrar" class="underline underline-offset-4 hover:text-primary">
+                    Cadastre-se
+                </NuxtLink>
+                <NuxtLink to="/lembrar" class="underline underline-offset-4 hover:text-primary">
+                    Lembrar Senha
+                </NuxtLink>
+            </p>
+        </UiCardFooter>
     </div>
 </template>
